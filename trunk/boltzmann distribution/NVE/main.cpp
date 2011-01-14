a713614cef101bac9b3f22596ae520a2b48a1309
rodolfofaria86@gmail.com <rodolfofaria86@gmail.com@05e249ec-c5b0-16b5-ad4b-e9c28bbe45ee>
2011-01-14 02:15:47 +0000

    - Changed the output file name; and - Added another initial energy distribution. Note: Improve code with functions.

--- trunk/boltzmann distribution/NVE/main.cpp
+++ trunk/boltzmann distribution/NVE/main.cpp
@@ -12,21 +12,28 @@ It uses the Microcanonical Ensemble (NVE)
 
 using namespace std;
 
-#define N 1000 //number of objects
-
 int main()
 {
 	ofstream file;
-	file.open("output.dat");
+	file.open("histogram.dat");
 
+	const int N = 1000; //number of objects
+	const int max_quanta = 35;
 	int quanta = 5;
+	int quanta_1 = 7;
+	int quanta_2 = 3;
 	int mat[ N ];
 	srand( (unsigned) time( 0 ) );
-	int cycle = 10000*N;
+	int cycle = 100*N;
 
 	//set all the object's energy to 5 quanta
-	for(int i = 0; i < N; i++)
-		mat[ i ] = quanta;
+	//for(int i = 0; i < N; i++)
+	//	mat[ i ] = quanta;
+	//Another initial condition
+	for(int i = 0; i < N/2; i++)
+		mat[ i ] = quanta_1;
+	for(int i = N/2; i < N; i++)
+		mat[ i ] = quanta_2;
 
 	for(int k = 0; k < cycle; k++)
 	{
@@ -45,22 +52,20 @@ int main()
 			mat[ object_2 ] += 1;
 
 			k -= 1; 
-		}			
+		}
 	}
 	
-	file << "Element" << setw( 13 ) << "Value" << endl;
+	file << "Quanta" << setw( 13 ) << "Frequency" << endl;
 
 	//output the final macrostate and verify the conservation of the total energy of the system:
 	int total_energy = 0;
 	for(int j = 0; j < N; j++)
-	{
-		file << setw( 7 ) << j + 1 << setw( 13 ) << mat[ j ] << endl;
 		total_energy += mat [ j ];
-	}
 	cout << total_energy << endl;
 
-	//compute the frequency in which each energy appears in the final macrostate till 35 quanta
-	for(int i = 0; i < 35; i++)
+	//compute the frequency in which each energy appears in the final macrostate till max_quanta
+	double density[ max_quanta ];
+	for(int i = 0; i < max_quanta; i++)
 	{
 		int frequency = 0;
 		for(int j = 0; j < N; j++)
@@ -68,7 +73,9 @@ int main()
 				if(mat[ j ] == i)
 					++frequency;
 			}
-		cout << "number of " << i << "'s : " << frequency << endl;	
+		density[ i ] = double(frequency)/N;
+		file << i + 1 << setw(13) << frequency << endl;
+		cout << "number of " << i + 1 << "'s : " << frequency << endl;	
 	}
 	
 	return 0;
